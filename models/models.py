227c80786d47fa0e1679edae43c24a859f54500c
root <l.bisiach@gmail.com>
2020-10-15 15:30:12 -0300

    Se agrego un boton que calcula los viaticos segun fecha y empleado

--- models/models.py
+++ models/models.py
@@ -2,6 +2,8 @@
 
 from odoo import models, fields, api
 
+import logging
+_logger = logging.getLogger(__name__)
 
 class tmsEmployeeWorks(models.Model):
     _inherit = 'hr.payslip'
@@ -9,6 +11,13 @@ class tmsEmployeeWorks(models.Model):
 
     ids_services = fields.Many2many('service.services', compute="_compute_data",string="Servicios")
     employee_tms = fields.Many2one('employee.services', string="Employee TMS")
+
+    qty_viatico_comida = fields.Float(string="Comida")
+    qty_viatico_especial = fields.Float(string="V. Especial")
+    qty_viatico_km = fields.Float(string="Km")
+    qty_viatico_ccyd = fields.Float(string="CCyD")
+    qty_viatico_km_he = fields.Float(string="KM H.E.")
+    qty_viatico_he = fields.Float(string="H.E.")
 
     @api.depends('date_from', 'date_to', 'employee_tms')
     def _compute_data(self):
@@ -21,3 +30,35 @@ class tmsEmployeeWorks(models.Model):
         # Traemos los servicios de dicha clase y los ordenamos por los valores que querramos, en caso de querer ordenar por mas valores, seguir con una coma ingresar el campo y el orden
         servicios = self.env['service.services'].search(servicios_domain, order='date_start asc')
         self.ids_services = servicios
+
+
+    def get_viaticos(self):
+        self.qty_viatico_comida = 0
+        self.qty_viatico_especial = 0
+        self.qty_viatico_km = 0
+        self.qty_viatico_ccyd = 0
+        self.qty_viatico_km_he = 0
+        self.qty_viatico_he = 0
+        if len(self.ids_services) == 0:
+            _logger.info("No hay servicios")
+            self.qty_viatico_comida = 0
+            self.qty_viatico_especial = 0
+            self.qty_viatico_km = 0
+        else:
+            _logger.info("Hay servicios")
+            for obj in self.ids_services:
+                for costos in obj.costos_ids:
+                    if costos.employee.ids == self.employee_tms.ids:
+                        _logger.info("El costo es: %s", costos.name.name)
+                        if costos.name.code == '4.1.12':
+                            self.qty_viatico_comida += costos.qty
+                        if costos.name.code == '4.1.13':
+                            self.qty_viatico_especial += costos.qty
+                        if costos.name.code == '4.2.14':
+                            self.qty_viatico_he += costos.qty
+                        if costos.name.code == '4.2.4':
+                            self.qty_viatico_km += costos.qty
+                        if costos.name.code == '4.2.3':
+                            self.qty_viatico_km_he += costos.qty
+                        if costos.name.code == '4.2.6':
+                            self.qty_viatico_ccyd += costos.qty
